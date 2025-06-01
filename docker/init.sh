28c883574e2913f866b1074532f4cfbb2da01dd3
Тимур <timurikvx@mail.ru>
2025-06-01 21:12:12 +0300

    docker compose

--- docker/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /var/www/elfsight
-composer install
-
-php /var/www/elfsight/bin/console doctrine:migrations:migrate --no-interaction -q
-php /var/www/elfsight/bin/console episode:import
-
-exec php-fpm -F
